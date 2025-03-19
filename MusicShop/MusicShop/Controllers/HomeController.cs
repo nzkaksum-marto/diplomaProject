662e1896ca8e843bbd651bfeb498c61c3114ed8e
nzkaksum-marto <martinmiroslavovpetrov@gmail.com>
2025-03-19 15:22:32 +0200

    slideshow

--- MusicShop/MusicShop/Controllers/HomeController.cs
+++ MusicShop/MusicShop/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
+using MusicShop.Core.Contracts;
 using MusicShop.Models;
+using MusicShop.Models.Product;
+
 using System.Diagnostics;
 
 namespace MusicShop.Controllers
@@ -7,15 +11,32 @@ namespace MusicShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<ProductIndexVM> products = _productService.GetProducts()
+                .Select(product => new ProductIndexVM
+                {
+                    Id = product.Id,
+                    ProductName = product.ProductName,
+                    BrandId = product.BrandId,
+                    BrandName = product.Brand.BrandName,
+                    CategoryId = product.CategoryId,
+                    CategoryName = product.Category.CategoryName,
+                    Picture = product.Picture,
+                    Quantity = product.Quantity,
+                    Price = product.Price,
+                    Discount = product.Discount
+
+                }).ToList();
+            return this.View(products);
         }
 
         public IActionResult Privacy()
